e1795ee0cec9c65519b1a448d22bc2defbfb9fff
andersonkaiti <anderkaiti@gmail.com>
2025-08-03 20:50:11 -0300

    ✨ feat(index): create a basic HTTP server in index.js that responds with "Hello, World!"

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import http from "node:http";
+
+const server = http.createServer((_request, response) => {
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.end("<h1>Hello, World!</h1>");
+});
+
+server.listen(3000, () =>
+  console.log(`🚀 Server started at http://localhost:3000`)
+);
