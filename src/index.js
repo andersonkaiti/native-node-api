ca65faaad0ded1a850095d19c9c2209b23c03f09
andersonkaiti <anderkaiti@gmail.com>
2025-08-03 20:56:18 -0300

    ✨ feat(users): add sorting functionality to user list based on query parameter

--- src/index.js
+++ src/index.js
@@ -2,20 +2,26 @@ import http from "node:http";
 import routes from "./routes.js";
 
 const server = http.createServer((request, response) => {
-  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`);
+
+  console.log(
+    `Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`
+  );
 
   const route = routes.find(
     (routeObj) =>
-      routeObj.endpoint === request.url && routeObj.method === request.method
+      routeObj.endpoint === parsedUrl.pathname &&
+      routeObj.method === request.method
   );
 
   if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
     route.handler(request, response);
     return;
   }
 
   response.writeHead(404, { "Content-Type": "text/html" });
-  response.end(`Cannot ${request.method} ${request.url}`);
+  response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
 });
 
 server.listen(3000, () =>
